8547bcd959a0be824a636dddc2d54c31afdea206
Anna <sozontova.an@icloud.com>
2026-06-22 20:06:59 +0700

    Initial Commit

---
+++ Ai/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Ai
+//
+//  Created by admin on 22.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
